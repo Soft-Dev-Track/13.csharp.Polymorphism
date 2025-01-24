06c7fba53c7bf5911b1b1ec68d8baf00e8c1a7c3
Pierre <46715488+pierrenoel@users.noreply.github.com>
2025-01-24 12:00:11 +0100

    update

---
+++ Solution/OPP_Advanced_Polymorphism/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OPP_Advanced_Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
